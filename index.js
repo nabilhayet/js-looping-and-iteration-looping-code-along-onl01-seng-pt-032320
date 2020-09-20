3a0141b9a99c58b9299c442a1c2633bc5b4f2b60
nabilhayet <nabil4457@yahoo.com>
2020-09-20 16:04:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+const names = ["teddy bear", "drone", "doll"];
+const message = []
+function writeCards(names, event) {
+  for (let i = 0; i < names.length; i++) {
+    message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+ 
+  return message;
+}
+
+function countDown(value) {
+    let i = value; // the initialization moved OUTSIDE the body of the loop!
+    while (i >= 0) {
+      console.log('${[i]}');
+      i --; // the iteration moves INSIDE the body of the loop!
+    }
+  }
